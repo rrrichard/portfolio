575bb97549c7230437fb7ea4342f821ecfa40dbf
rrrichard <richard.rabulan@hotmail.co.uk>
2019-03-18 15:47:08 +0000

    tests the 2 functions that adds the database to the front end

---
+++ php/tests.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once  'functions.php';
+
+use PHPUnit\Framework\Testcase;
+
+class FunctionTest extends Testcase
+{
+    public function testAddParagraphsSuccess(){
+        $expected = '<p>Hello World</p>';
+        $input = [['paragraph'=>'Hello World']];
+        $case = addParagraphs($input);
+        $this->assertEquals($expected, $case);
+    }
+
+    public function testAddParagraphsFailure(){
+        $expected = '<p></p>';
+        $input = [['paragraph' => '']];
+        $case = addParagraphs($input);
+        $this->assertEquals($expected,$case);
+    }
+
+    public function testAddParagraphsMalformed(){
+        $input = 3;
+        $this->expectException(TypeError::class);
+        addParagraphs($input);
+    }
+
+}
