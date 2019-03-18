6c4d36275c1abfdb4f799807a39bbfacf5e47414
rrrichard <richard.rabulan@hotmail.co.uk>
2019-03-18 15:46:35 +0000

    added things that will catch unit testing failures

--- php/functions.php
+++ php/functions.php
@@ -1,8 +1,13 @@
 <?php
 
 
-
-
+/**
+ * this function selects the paragraphs in the database and fetches all the data
+ *
+ * @param $db is the variable that connects the php to mysql and allows it 
+ *
+ * @return mixed
+ */
 function addAboutMe($db)
 {
     $query = $db->prepare("SELECT `paragraph` FROM `about_me`");
@@ -10,10 +15,14 @@ function addAboutMe($db)
     return $query->fetchAll();
 }
 
-function addParagraphs($paragraphs){
-    $paragraphPlaceholder = '';
-    foreach ($paragraphs as $paragraph){
-        $paragraphPlaceholder .= '<p>' . $paragraph['paragraph'] . '</p>';
-    }
-    return $paragraphPlaceholder;
+function addParagraphs(array $paragraphs) :string {
+    if (is_array($paragraphs)) {
+
+        $paragraphPlaceholder = '';
+        foreach ($paragraphs as $paragraph) {
+            $paragraphPlaceholder .= '<p>' . $paragraph['paragraph'] . '</p>';
+        }
+        return $paragraphPlaceholder;
+    } else return 'You must enter an array';
+
 }
