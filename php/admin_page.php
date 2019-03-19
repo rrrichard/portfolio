e0a66ffe7562a1a78f0de1773c35927b8e5057eb
rrrichard <richard.rabulan@hotmail.co.uk>
2019-03-19 15:47:07 +0000

    added the php link to the text area and made variables to call some functions

--- php/admin_page.php
+++ php/admin_page.php
@@ -4,9 +4,26 @@ require_once '../db/db_query.php';
 //require_once 'admin_edit.php';
 //$editParagraphs = editDropdown($db);
 $db = getDbConnection();
-
+$editChoice = $_POST['editSelect'];
 $paragraphs = addAboutMe($db);
 $editDropdownResults = editParagraphDropdown($paragraphs);
+
+$getEdit = getEdit($db, $editChoice);
+$pasteEdit = pasteEdit($getEdit);
+var_dump($editChoice);
+var_dump($getEdit);
+
+
+//if (isset($editChoice)){
+//    $query = $db->prepare("SELECT `paragraph` FROM `about_me` WHERE `id` = :choiceId");
+//    $query->bindParam(':choiceId',$editDropdownResults);
+//    $query->execute();
+//    return $query->fetchAll();
+//}
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -36,8 +53,9 @@ $editDropdownResults = editParagraphDropdown($paragraphs);
         </div>
         <div class="formSize">
             <p>Edit paragraph</p>
-            <form method="post" action="admin_edit.php">
-                <textarea rows="6" cols="80" name="edit_form"></textarea>
+            <form method="post" action="admin_page.php">
+                <textarea rows="6" cols="80" name="edit_form"><?php echo $pasteEdit; ?></textarea>
+
                 <h4>Choose which paragraph to edit</h4>
                 <select name="editSelect">
                     <?php echo $editDropdownResults; ?>
