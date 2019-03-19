140d5b36254a7e91f2bf3e550b18082b0f78ead8
rrrichard <richard.rabulan@hotmail.co.uk>
2019-03-19 10:11:19 +0000

    minor changes with cleaner looking code

--- php/functions.php
+++ php/functions.php
@@ -8,33 +8,21 @@
  *
  * @return mixed returns all the data in order of their id
  */
-function addAboutMe($db)
+function addAboutMe(PDO $db)
 {
-    $query = $db->prepare("SELECT `paragraph` FROM `about_me`");
+    $query = $db->prepare("SELECT `paragraph` FROM `about_me` WHERE `deleted` = '0';");
     $query->execute();
     return $query->fetchAll();
 }
 
-//
-///**
-// * this function enables the data to be returned as a <p> string in the website
-// *
-// * @param array $paragraphs calls in the method that contains all the arrays of the paragraphs
-// *
-// * @return string returns each paragraph in the database as a <p> string and enables it to be output in the website
-// */
-//function addParagraphs(array $paragraphs) :string {
-//    if (is_array($paragraphs)) {
-//
-//        $paragraphPlaceholder = '';
-//        foreach ($paragraphs as $paragraph) {
-//            $paragraphPlaceholder .= '<p>' . $paragraph['paragraph'] . '</p>';
-//        }
-//        return $paragraphPlaceholder;
-//    } else return 'You must enter an array';
-//
-//}
 
+/**
+ * this function enables the data to be returned as a <p> string in the website
+ *
+ * @param array $paragraphs calls in the method that contains all the arrays of the paragraphs
+ *
+ * @return string returns each paragraph in the database as a <p> string and enables it to be output in the website
+ */
 function addParagraphs(array $paragraphs) :string {
 
 
