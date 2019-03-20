44df9c5bed8f2b4bbd7244b2e67dd8318b9c0718
rrrichard <richard.rabulan@hotmail.co.uk>
2019-03-20 21:36:47 +0000

    added a php function that allows the submit button for edit to only show when edit button is clicked

--- php/admin_add.php
+++ php/admin_add.php
@@ -3,10 +3,10 @@
 require_once 'functions.php';
 require_once '../db/db_query.php';
 
-
 $addSubmit = $_POST['add_form'];
 $db = getDbConnection();
 addParagraphToDb($db, $addSubmit);
+
 ?>
 
 Your new paragraph has been successfully added, click <a href="admin_page.php">here</a> if you want to do more.
