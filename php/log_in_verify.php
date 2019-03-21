0a4513735d69c4ad4e20a6cc001792f4e4985d92
rrrichard <richard.rabulan@hotmail.co.uk>
2019-03-21 15:33:57 +0000

    created a condition that checks if the id and password exists in the system

---
+++ php/log_in_verify.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once 'functions.php';
+require_once '../db/db_query.php';
+
+$db = getDbConnection();
+$postUsername = $_POST['username'];
+$postPassword = $_POST['password'];
+
+if (isset($_POST['username'])){
+    $postUsername = $_POST['username'];
+    $test = usernameVerify($db, $postUsername, $postPassword);
+    if ($test == true || $_SESSION == true){
+        $_SESSION['valid'] = true;
+        header('Location: admin_page.php');
+    } else {
+        header('Location: log_in.php');
+    }
+}
+
+
+
+
+
