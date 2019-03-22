58ee4d8d03575335df33f54d6f5f6a7674d6caf8
rrrichard <richard.rabulan@hotmail.co.uk>
2019-03-22 10:39:50 +0000

    no changes, pressed save

--- php/log_in_verify.php
+++ php/log_in_verify.php
@@ -4,12 +4,16 @@ require_once 'functions.php';
 require_once '../db/db_query.php';
 
 $db = getDbConnection();
+$logInDetails = getDetails($db);
+$dbUsername = $logInDetails['username'];
+$dbPassword = $logInDetails['password'];
+$passwordCheck = password_verify($_POST['password'], $dbPassword);
 
-if (isset($_POST['username']) && isset($_POST['password'])){
+if (isset($_POST['username']) && isset($_POST['password'])) {
     $postUsername = $_POST['username'];
     $postPassword = $_POST['password'];
-    $test = usernameVerify($db, $postUsername, $postPassword);
-    if ($test == true){
+    $passwordCheck = password_verify($_POST['password'], $dbPassword);
+    if ($passwordCheck == true) {
         $_SESSION['valid'] = true;
         header('Location: admin_page.php');
     } else {
